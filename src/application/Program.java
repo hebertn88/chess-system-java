585469e1b7ea41ccb5d2316de1f97c9916952c5c
Hebert do Nascimento <hebertn88@gmail.com>
2023-04-03 12:47:35 -0300

    class Position created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,11 @@
-package application;public class Program {
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        var pos = new Position(1,2);
+
+        System.out.println(pos);
+    }
 }
